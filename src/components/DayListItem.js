ae33f129436c8b508b3dc74675f4bef770f7ed95
penguinboots <sabrina.ynw@gmail.com>
2023-03-27 12:24:20 -0700

    set # spots message

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,17 +6,33 @@ export default function DayListItem(props) {
   let dayClass = classNames(
     'day-list__item',
     {
-      'day-list__item--selected' : props.selected,
-      'day-liste__item--full' : props.spots === 0
+      'day-list__item--selected': props.selected,
+      'day-liste__item--full': props.spots === 0
     }
   )
 
+  let formatSpots = (spots) => {
+    let message = '';
+    switch (spots) {
+      case 0:
+        message = 'no spots remaining';
+        break;
+      case 1:
+        message = '1 spot remaining';
+        break;
+      default:
+        message = `${spots} spots remaining`
+    }
+    return message;
+  }
+
+
   return (
     <li
-    className={dayClass} 
-    onClick={() => props.setDay(props.name)}>
+      className={dayClass}
+      onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
