73e92676cb833126153226e4a00eb67835968add
penguinboots <sabrina.ynw@gmail.com>
2023-04-01 10:01:06 -0700

    feat: render appointments on main page

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from "components/Appointment";
 
 const days = [
   {
@@ -20,6 +21,54 @@ const days = [
     spots: 0,
   },
 ];
+
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
+const schedule = Object.values(appointments).map((appointment) => {
+  return (
+    <Appointment
+      key={appointment.key}
+      {...appointment}
+    />
+  );
+});
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
@@ -45,7 +94,8 @@ export default function Application(props) {
           alt="Lighthouse Labs"
         />      </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {schedule}
+        <Appointment key='last' time='5pm' />
       </section>
     </main>
   );
