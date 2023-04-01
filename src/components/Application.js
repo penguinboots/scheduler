0c960748d4c326ec29ee77c6988d17419d9ded25
penguinboots <sabrina.ynw@gmail.com>
2023-04-01 12:50:02 -0700

    feat: combine states

--- src/components/Application.js
+++ src/components/Application.js
@@ -54,12 +54,19 @@ const schedule = Object.values(appointments).map((appointment) => {
 });
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
-  const [days, setDays] = useState([]);
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     axios.get('/api/days').then((response) => {
-      setDays(response.data);
+      setDays(response.data)
     })
   })
 
@@ -74,8 +81,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
