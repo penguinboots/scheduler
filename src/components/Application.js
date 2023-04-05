bc03bae6f5b3c9b128f03cc1519d1c27d4b872ed
penguinboots <sabrina.ynw@gmail.com>
2023-04-05 11:22:25 -0700

    feat: add interviewer data to state

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,7 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 import axios from "axios";
-import { getAppointmentsForDay } from "helpers/selector";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 
 export default function Application(props) {
@@ -12,18 +12,21 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   useEffect(() => {
     Promise.all([
     axios.get('/api/days'),
-    axios.get('/api/appointments')])
+    axios.get('/api/appointments'),
+    axios.get('/api/interviewers')])
     .then((all) => {
       setState(prev => ({
         ...prev,
         days: all[0].data,
-        appointments: all[1].data
+        appointments: all[1].data,
+        interviewers: all[2].data
       }))
     })
   })
@@ -31,12 +34,15 @@ export default function Application(props) {
   const setDay = day => setState({ ...state, day });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
-
   const schedule = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
     return (
       <Appointment
         key={appointment.id}
-        {...appointment}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
       />
     );
   });
