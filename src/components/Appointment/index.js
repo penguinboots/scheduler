aafc9314c0fc61ed5528d8ffb21fa8706b097671
penguinboots <sabrina.ynw@gmail.com>
2023-04-05 13:18:45 -0700

    feat: add load screen

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,12 +5,14 @@ import Show from "./Show";
 import Empty from "./Empty";
 import useVisualMode from "hooks/useVisualMode";
 import Form from "./Form";
+import Status from "./Status";
 
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -21,6 +23,9 @@ export default function Appointment(props) {
       student: name,
       interviewer
     };
+
+    transition(SAVING);
+
     props.bookInterview(props.id, interview)
     .then(() => transition(SHOW));
   }
@@ -41,6 +46,8 @@ export default function Appointment(props) {
           onCancel={() => back()}
           onSave={save}
         />}
+      {mode === SAVING &&
+        <Status />}
     </article>
   );
 }
