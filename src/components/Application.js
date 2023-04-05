6882eda3f8d356f5b818cc4f7ad3d91e9035ea09
penguinboots <sabrina.ynw@gmail.com>
2023-04-05 13:16:20 -0700

    feat: add PUT request

--- src/components/Application.js
+++ src/components/Application.js
@@ -42,7 +42,9 @@ export default function Application(props) {
       ...state.appointments,
       [id]: appointment
     }
-    setState({ ...state, appointments });
+
+    return axios.put(`/api/appointments/${id}`, appointment)
+      .then(setState({ ...state, appointments }))
   }
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
