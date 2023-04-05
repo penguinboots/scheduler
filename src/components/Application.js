7e6fd47e5d3da61be75960f351fbdd61e408948c
penguinboots <sabrina.ynw@gmail.com>
2023-04-05 14:21:12 -0700

    refactor: remove unnecessary body for axios req

--- src/components/Application.js
+++ src/components/Application.js
@@ -56,7 +56,7 @@ export default function Application(props) {
       ...state.appointments,
       [id]: appointment
     }
-    return axios.delete(`/api/appointments/${id}`, appointment)
+    return axios.delete(`/api/appointments/${id}`)
       .then(setState({ ...state, appointments }))
   }
 
