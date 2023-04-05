a48aa39057524161e3d96cfe8901385a7c0bb06b
penguinboots <sabrina.ynw@gmail.com>
2023-04-05 11:21:45 -0700

    fix: file rename

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -12,3 +12,12 @@ export function getAppointmentsForDay(state, day) {
 
   return appts;
 }
+
+export function getInterview(state, interview) {
+  if (!interview) return null;
+  let filteredInterview = {
+    student: interview.student,
+    interviewer: state.interviewers[interview.interviewer]
+  };
+  return filteredInterview;
+}
