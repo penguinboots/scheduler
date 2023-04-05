1ad458ad5f1fe007414f73d66df605568f89e3d3
penguinboots <sabrina.ynw@gmail.com>
2023-04-05 12:09:26 -0700

    feat: add getInterviewersForDay

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -13,6 +13,21 @@ export function getAppointmentsForDay(state, day) {
   return appts;
 }
 
+export function getInterviewersForDay(state, dayOf) {
+  const filteredDays = state.days.filter(day => day.name === dayOf);
+  if (state.days.length === 0 | filteredDays.length === 0) {
+    return [];
+  }
+
+  let interviewerIDs = filteredDays[0].interviewers;
+
+  let interviewers = [];
+  for (const intID of interviewerIDs) {
+    interviewers.push(state.interviewers[intID]);
+  }
+  return interviewers;
+}
+
 export function getInterview(state, interview) {
   if (!interview) return null;
   let filteredInterview = {
