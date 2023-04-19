e8cf3f55e08c4fcd00ffccd1c25a9b5c5419f0f7
penguinboots <sabrina.ynw@gmail.com>
2023-04-19 11:53:34 -0700

    add test templates

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, getByPlaceholderText, getByTestId } from "@testing-library/react";
+import { render, cleanup, getByPlaceholderText, getByTestId, fireEvent } from "@testing-library/react";
 
 import Form from "components/Appointment/Form";
 
@@ -30,5 +30,39 @@ describe("Form", () => {
       />
     );
     expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
+  });
+
+  it("validates that the student name is not blank", () => {
+    const onSave = jest.fn();
+
+    const { getByText } = render(
+      <Form
+        interviewers={interviewers}
+        onSave={onSave}
+      />
+    )
+    fireEvent.click(getByText("Save"));
+    expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
+    expect(onSave).not.toHaveBeenCalled();
+  });
+
+  it("validates that the interviewer cannot be null", () => {
+    /* 3. validation is shown */
+    expect(getByText(/please select an interviewer/i)).toBeInTheDocument();
+
+    /* 4. onSave is not called */
+    expect(onSave).not.toHaveBeenCalled();
+  });
+
+  it("calls onSave function when the name is defined", () => {
+    /* 5. validation is not shown */
+    expect(queryByText(/student name cannot be blank/i)).toBeNull();
+    expect(queryByText(/please select an interviewer/i)).toBeNull();
+
+    /* 6. onSave is called once*/
+    expect(onSave).toHaveBeenCalledTimes(1);
+
+    /* 7. onSave is called with the correct arguments */
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
   });
 });
